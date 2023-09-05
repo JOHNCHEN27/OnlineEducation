8dc26851f0d3cf1af697054d6909949fd7f9b0a7
JOHNCHEN27 <2502523383@qq.com>
2023-09-05 15:34:43 +0800

    内容管理模块功能设计

---
+++ OnlineEducation-Content/Content-api/src/main/java/com/lncanswer/content/api/CourseBaseInfoController.java
@@ -0,0 +1,48 @@
+package com.lncanswer.content.api;
+
+import com.lncanswer.base.model.PageParams;
+import com.lncanswer.base.model.PageResult;
+import com.lncanswer.model.dto.QueryCourseParamsDto;
+import com.lncanswer.model.po.CourseBase;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author LNC
+ * @version 1.0
+ * @date 2023/9/5 13:53
+ */
+@Api(value = "课程信息编辑接口",tags = "课程信息编辑接口") //swagger接口文档说明
+@RestController
+public class CourseBaseInfoController {
+
+    /**
+     * 查询课程信息分页接口   分页信息用PageParams实体类封装接收，
+     * 具体查询信息用QueryCourseParamsDto 实体类封装接收 注解为JSON格式
+     * @param pageParams
+     * @param queryCourseParamsDto
+     * @return
+     */
+    @ApiOperation("课程查询接口") //对此方法的接口描述
+    @PostMapping ("/course/list")//RequestMapping会生成所有类型的接口上传方法
+    public PageResult<CourseBase> list    //required = false 表示此参数不是必填项
+            (PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto){
+        //利用Swagger进行接口测试
+        CourseBase courseBase =new CourseBase();
+        courseBase.setName("测试名称");
+        courseBase.setCreateDate(LocalDateTime.now());
+        List<CourseBase> courseBases = new ArrayList<>();
+        courseBases.add(courseBase);
+        PageResult<CourseBase> pageResult = new PageResult<>(courseBases,10,1,10);
+
+           return pageResult;
+    }
+
+}
---
+++ OnlineEducation-generator/src/main/java/com/lncanswer/content/service/CourseCategoryService.java
@@ -0,0 +1,16 @@
+package com.lncanswer.content.service;
+
+import com.lncanswer.content.model.po.CourseCategory;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程分类 服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2022-10-08
+ */
+public interface CourseCategoryService extends IService<CourseCategory> {
+
+}
