d51051e5fb9fba1674296adce3835fd9596cd661
JOHNCHEN27 <2502523383@qq.com>
2023-10-23 09:10:41 +0800

    Gateway认证

--- xuecheng-plus-auth/src/main/java/com/xuecheng/auth/config/TokenConfig.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/auth/config/TokenConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 /**
  * @author Administrator
@@ -14,15 +18,34 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
  **/
 @Configuration
 public class TokenConfig {
+    //签名密钥
+    private String SIGNING_KEY = "mq123";
 
     @Autowired
     TokenStore tokenStore;
 
+//    @Bean
+//    public TokenStore tokenStore() {
+//        //使用内存存储令牌（普通令牌）
+//        return new InMemoryTokenStore();
+//    }
+
+    @Autowired
+    private JwtAccessTokenConverter accessTokenConverter;
+
+    //配置JWT令牌
     @Bean
-    public TokenStore tokenStore() {
-        //使用内存存储令牌（普通令牌）
-        return new InMemoryTokenStore();
+    public TokenStore tokenStore(){
+        return new JwtTokenStore(accessTokenConverter());
     }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SIGNING_KEY);
+        return converter;
+    }
+
 
     //令牌管理服务
     @Bean(name="authorizationServerTokenServicesCustom")
@@ -30,6 +53,9 @@ public class TokenConfig {
         DefaultTokenServices service=new DefaultTokenServices();
         service.setSupportRefreshToken(true);//支持刷新令牌
         service.setTokenStore(tokenStore);//令牌存储策略
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(accessTokenConverter));
+        service.setTokenEnhancer(tokenEnhancerChain);
         service.setAccessTokenValiditySeconds(7200); // 令牌默认有效期2小时
         service.setRefreshTokenValiditySeconds(259200); // 刷新令牌默认有效期3天
         return service;
